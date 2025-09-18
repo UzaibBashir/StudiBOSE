7ee46c1f7fd42192784739d77052cb1d572f48d6
UzaibBashir <abiuzaib@gmail.com>
2025-09-18 09:56:20 +0530

    removed community option

--- src/shared/Navigation.jsx
+++ src/shared/Navigation.jsx
@@ -7,7 +7,7 @@ export default function Navigation() {
       <NavLink to="/notes" className={({isActive}) => `flex flex-col items-center py-4 rounded-xl text-base ${isActive ? 'bg-white/20' : ''}`}>Notes</NavLink>
       <NavLink to="/papers" className={({isActive}) => `flex flex-col items-center py-4 rounded-xl text-base ${isActive ? 'bg-white/20' : ''}`}>Papers</NavLink>
       <NavLink to="/books" className={({isActive}) => `flex flex-col items-center py-4 rounded-xl text-base ${isActive ? 'bg-white/20' : ''}`}>Books</NavLink>
-      <NavLink to="/community" className={({isActive}) => `flex flex-col items-center py-4 rounded-xl text-base ${isActive ? 'bg-white/20' : ''}`}>Community</NavLink>
+
     </>
   )
 }
